542efc956378587a3d1634157e271a085a764f5f
DamiFuma02 <damianofumagalli2@gmail.com>
2021-11-22 21:50:11 +0100

    Architettura.html UPDATE

--- PROGRAMMAZIONE/esercizi laboratorio/es4.rkt
+++ PROGRAMMAZIONE/esercizi laboratorio/es4.rkt
@@ -57,6 +57,118 @@
 )
 
 
+;restituisce la cifra delle unità della somma tra i 3 caratteri inseriti
+(define btr-digit-sum                    ; val:     carattere +/./-
+  (lambda (u v c)                        ; u, v, c: caratteri +/./-
+    (cond ((char=? u #\-)                ; u v c
+           (cond ((char=? v #\-)
+                  (cond ((char=? c #\-)  ; - - -
+                         #\.)
+                        ((char=? c #\.)  ; - - .
+                         #\+)
+                        ((char=? c #\+)  ; - - +
+                         #\-)))
+                 ((char=? v #\.)
+                  (cond ((char=? c #\-)  ; - . -
+                         #\+)
+                        ((char=? c #\.)  ; - . .
+                         #\-)
+                        ((char=? c #\+)  ; - . +
+                         #\.)))
+                 ((char=? v #\+)         ; - + c
+                  c)))
+          ((char=? u #\.)
+           (cond ((char=? v #\-)
+                  (cond ((char=? c #\-)  ; . - -
+                         #\+)
+                        ((char=? c #\.)  ; . - .
+                         #\-)
+                        ((char=? c #\+)  ; . - +
+                         #\.)))
+                 ((char=? v #\.)         ; . . c
+                  c)
+                 ((char=? v #\+)
+                  (cond ((char=? c #\-)  ; . + -
+                         #\.)
+                        ((char=? c #\.)  ; . + .
+                         #\+)
+                        ((char=? c #\+)  ; . + +
+                         #\-)))))
+          ((char=? u #\+)
+           (cond ((char=? v #\-)         ; + - c
+                  c)
+                 ((char=? v #\.)
+                  (cond ((char=? c #\-)  ; + . -
+                         #\.)
+                        ((char=? c #\.)  ; + . .
+                         #\+)
+                        ((char=? c #\+)  ; + . +
+                         #\-)))
+                 ((char=? v #\+)
+                  (cond ((char=? c #\-)  ; + + -
+                         #\+)
+                        ((char=? c #\.)  ; + + .
+                         #\-)
+                        ((char=? c #\+)  ; + + +
+                         #\.)))))
+          )))
+
+(define btr-carry                    ; val:     carattere +/./-
+  (lambda (u v c)                        ; u, v, c: caratteri +/./-
+    (cond ((char=? u #\-)                ; u == -
+           (cond ((char=? v #\-)
+                  (cond ((char=? c #\-)  ; - - -
+                         #\-)
+                        ((char=? c #\.)  ; - - .
+                         #\-)
+                        ((char=? c #\+)  ; - - +
+                         #\.)))
+                 ((char=? v #\.)
+                  (cond ((char=? c #\-)  ; - . -
+                         #\-)
+                        ((char=? c #\.)  ; - . .
+                         #\.)
+                        ((char=? c #\+)  ; - . +
+                         #\.)))
+                 ((char=? v #\+)         ; - + c
+                  #\.)))
+          ((char=? u #\.)
+           (cond ((char=? v #\-)
+                  (cond ((char=? c #\-)  ; . - -
+                         #\-)
+                        ((char=? c #\.)  ; . - .
+                         #\.)
+                        ((char=? c #\+)  ; . - +
+                         #\.)))
+                 ((char=? v #\.)         ; . . c
+                  c)
+                 ((char=? v #\+)
+                  (cond ((char=? c #\-)  ; . + -
+                         #\.)
+                        ((char=? c #\.)  ; . + .
+                         #\.)
+                        ((char=? c #\+)  ; . + +
+                         #\+)))))
+          ((char=? u #\+)
+           (cond ((char=? v #\-)         ; + - c
+                  #\.)
+                 ((char=? v #\.)
+                  (cond ((char=? c #\-)  ; + . -
+                         #\.)
+                        ((char=? c #\.)  ; + . .
+                         #\.)
+                        ((char=? c #\+)  ; + . +
+                         #\+)))
+                 ((char=? v #\+)
+                  (cond ((char=? c #\-)  ; + + -
+                         #\.)
+                        ((char=? c #\.)  ; + + .
+                         #\+)
+                        ((char=? c #\+)  ; + + +
+                         #\+)))))
+          )))
+
+
 (define sum  ;restitusce una stringa "carry + somma"
   (lambda (a1 a2 c)
     (let ((add (btr-rep (+ (string-number a1) (string-number a2) (string-number c)) )))
@@ -67,6 +179,19 @@
     )
   )
 )
+
+
+(define checksum
+  (lambda (a b c)
+       (string-append
+                (btr-carry a b c)
+                (btr-digit-sum a b c)
+       )
+  )
+)
+
+
+
 (define somma    ; fa la somma di due stringhe lunghe uguali
   (lambda (add1 add2 c)
           (let ((k1 (-(string-length add1)1)) (k2 (-(string-length add2)1)))
@@ -96,35 +221,54 @@
 )
 
 
+
+(define normalized-btr   ;rimuove gli 0 più significativi
+   (lambda (str)
+       (btr-rep    ;converte il valore decimale in stringa BTR per elimiare gli 0 più significativi
+           (converti ;converte la stringa BTR in numero decimale
+              str  
+           )
+      )
+   )
+)
+
+
 (define btr-sum
      (lambda (add1 add2)
         (let ((k1 (string-length add1)) (k2 (string-length add2)))
           (cond
             ((= k1 k2)
                    ;le due stringhe hanno la stessa lunghezza perciò si esegue la somma normalmente
-                   (btr-rep    ;converte il valore decimale in stringa BTR per elimiare gli 0 più significativi
-                        (converti ;converte il risultato ottenuto in valore decimale
-                             (somma add1 add2 ".")   ; il primo riporto va impostato come nullo
-                         )
-                   )
+                   (normalized-btr (somma add1 add2 "."))
             )
             ;se non sono uguali si aggiunge (kMag - Kmin) "." a SX della stringa più corta
             ((< k1 k2)     ; add2 < add1 perciò si allunga add2
-                (btr-rep    ;converte il valore decimale in stringa BTR per elimiare gli 0 più significativi
-                        (converti ;converte il risultato ottenuto in valore decimale
-                             (somma (aggiungi add1 (- k2 k1)) add2 ".")   ; il primo riporto va impostato come nullo
-                         )
-                )  
-            )
+                  (normalized-btr (somma (aggiungi add1 (- k2 k1)) add2 "."))
+             )
             ((< k2 k1)  ; add2 < add1 perciò si allunga add2
-                  (btr-rep    ;converte il valore decimale in stringa BTR per elimiare gli 0 più significativi
-                        (converti ;converte il risultato ottenuto in valore decimale
-                             (somma (aggiungi add2 (- k1 k2)) add1 ".")   ; il primo riporto va impostato come nullo
-                         )
-                  )
+                  (normalized-btr (somma (aggiungi add2 (- k1 k2)) add1 "."))
             )
-           )
+          )
         )
      )
 )
  
+(define btr-carry-sum
+     (lambda (add1 add2 c)
+        (let ((k1 (string-length add1)) (k2 (string-length add2)))
+          (cond
+            ((= k1 k2)
+                   ;le due stringhe hanno la stessa lunghezza perciò si esegue la somma normalmente
+                   (normalized-btr (somma add1 add2 "."))
+            )
+            ;se non sono uguali si aggiunge (kMag - Kmin) "." a SX della stringa più corta
+            ((< k1 k2)     ; add2 < add1 perciò si allunga add2
+                  (normalized-btr (somma (aggiungi add1 (- k2 k1)) add2 "."))
+             )
+            ((< k2 k1)  ; add2 < add1 perciò si allunga add2
+                  (normalized-btr (somma (aggiungi add2 (- k1 k2)) add1 "."))
+            )
+          )
+        )
+     )
+)
